850516a9c8c217528cf776a0792ee93d827145e2
Anis Benyoub intern CNRS Liris <stagiaire@liristso.(none)>
2012-06-25 15:51:23 +0200

    Add template polymorphism

---
+++ TestPolymorphism/TestPolymorphism/DGtalObjectContainer.h
@@ -0,0 +1,36 @@
+	
+#include <string.h>
+#include <iostream>
+using namespace std;	
+	
+	
+
+	class TDrawableWithOgre
+	{
+	  public:
+	    TDrawableWithOgre( ) { }
+	    ~TDrawableWithOgre() { }
+	    virtual const char * className() const { return "a";}
+	protected:
+	  int typeClass;
+	};
+
+
+
+
+
+	template <class T> 
+	class TDrawable : public TDrawableWithOgre
+	{
+	  public:
+	    TDrawable()  { }
+	    ~TDrawable()  { }
+	 
+	    virtual const char * className() const
+	    {
+	        return  (static_cast <T*>(myObject)->className()).c_str(); 
+	    }
+	protected :
+	  T * myObject;
+
+	};
